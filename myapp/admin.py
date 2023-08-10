43149ba2d0698ee1fa7d98e156d14e65bc616022
Sachin Renjith <sachinranjith012gmail.com>
2023-08-10 18:15:16 +0530

    'shell'

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from myapp.models import Account
+from myapp.models import Teacher,Course,Chapter,Lesson
 # Register your models here.
-admin.site.register(Account)
+ 
+admin.site.register(Teacher)
+admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(Chapter)
--- myapp/models.py
+++ myapp/models.py
@@ -1,11 +1,42 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
-class Account(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    username = models.CharField(max_length=20, unique= False)
-    email = models.EmailField(max_length=30, unique=False)
+# class User(models.Model):
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     username = models.CharField(max_length=20, unique= False)
+#     email = models.EmailField(max_length=30, unique=False)
+
+#     def __str__(self):
+#         return self.first_name
+    
+class Teacher(models.Model):
+    teacher_name = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.teacher_name
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=30)
+    teacher = models.ForeignKey(Teacher,on_delete=models.CASCADE)
+    price = models.IntegerField()
 
     def __str__(self):
-        return self.first_name
+        return self.course_name
+
+class Lesson(models.Model):
+    lesson_name = models.CharField(max_length=30)
+    course = models.ForeignKey(Course,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.lesson_name
+
+class Chapter(models.Model):
+    chapter_name = models.CharField(max_length=30)
+    lesson = models.ForeignKey(Lesson,on_delete=models.CASCADE)
+    index_number = models.IntegerField() 
+
+    def __str__(self):
+        return self.chapter_name       
+
+
